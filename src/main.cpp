866a0c7993311d4e43efc73e112218a49495c20b
zhl <lzheng@frba.utn.edu.ar>
2026-04-22 02:20:59 -0300

    feat(dummy): añadir contador para el bucle principal en lugar de valores aleatorios, me permite debuggear la GUI de la GSE.

--- src/main.cpp
+++ src/main.cpp
@@ -9,9 +9,14 @@ void setup() {
     delay(10); // will pause mcu until serial console opens
 }
 
+long long counter = 0;
+
 void loop() {
-  SerialPrint::plot("rand0", static_cast<float>(esp_random()));
-  SerialPrint::plot("rand1", static_cast<float>(esp_random()));
-  SerialPrint::plot("rand2", static_cast<float>(esp_random()));
+
+  counter++;
+  SerialPrint::plot("counter", static_cast<float>(counter));
+
+  // SerialPrint::plot("rand1", static_cast<float>(esp_random()));
+  // SerialPrint::plot("rand2", static_cast<float>(esp_random()));
   delay(10);
 }
