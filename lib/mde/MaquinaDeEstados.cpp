680683f6f074cce6fc3b63f70b27a782f8a8d359
zhl <lzheng@frba.utn.edu.ar>
2026-05-27 16:03:38 -0300

    refactor(feat/mde): eliminar código obsoleto en bucle principal y preparar estructura para máquina de estados

---
+++ lib/mde/MaquinaDeEstados.cpp
@@ -0,0 +1,5 @@
+//
+// Created by zhl on 5/19/26.
+//
+
+#include "MaquinaDeEstados.h"
---
+++ lib/mde/MaquinaDeEstados.h
@@ -0,0 +1,13 @@
+//
+// Created by zhl on 5/19/26.
+//
+
+#ifndef ACEMA_CTLR_MAQUINADEESTADOS_H
+#define ACEMA_CTLR_MAQUINADEESTADOS_H
+
+
+class MaquinaDeEstados {
+};
+
+
+#endif //ACEMA_CTLR_MAQUINADEESTADOS_H
