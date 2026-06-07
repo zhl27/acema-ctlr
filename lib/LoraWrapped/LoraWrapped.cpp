a9b5f5dec887027e92fbabe71802945bb9a3cd15
Joel Nahuel Cruz <jocruz@frba.utn.edu.ar>
2026-06-06 22:08:24 -0300

    Fix: arreglo de sintaxis y subestados internos de conexion

--- lib/LoraWrapped/LoraWrapped.cpp
+++ lib/LoraWrapped/LoraWrapped.cpp
@@ -6,6 +6,7 @@ LoraWrapped::LoraWrapped(int ss, int reset, int dio0, SPIClass& spi)
 {
     LoRa.setPins(ss, reset, dio0);
     LoRa.setSPI(spi);
+    _st = CONNECTION_STATUS::DISCONNECTED;
 }
 
 LoraWrapped::~LoraWrapped()
@@ -21,10 +22,40 @@ bool LoraWrapped::begin(int sw, char ew, long frec){
     return false;
 }
 
+// PARALELISMO: TPC
 /*
 * Para mandar un paquete se debe enviar de manera ordenada la siguiente trama:
 * len (1 byte) -> protocol (1 byte) -> payload (len bytes)
+* protocolo entre Lora's 
+* paquete = (len, proto, payload)
 */
+
+
+// HTTP
+/*
+	protocolo entre ESP_gse <-> compu (gse)
+	esp del gse recibe la struct (bytes), y lo manda por serial con este formato:
+	
+	 '$'<Bytes(paquete 100)>'#'     '$'<Bytes(paquete 30)>'#'      '$'<Bytes(paquete 30)>'#'       '$'<Bytes(pa quete 80)>'#'  
+	
+	// IMPLEMENTACION SEGURA CON MAQUINA DE ESTADOS
+	cohete -> gse: '$''<long N>'
+	esperando_pesos -- si Byte[0] == '$' && byte[1] > 0  / envio 'ok'  -> recibiendo N (leer N bytes)
+	                                             <---
+	gse(serial) -> 'ok'
+	
+	enviar_pesos --> esperar 'ok' (leyendo sensor, mde, control vuelo) --> enviar N bytes (envio siguiente dato)
+					<---(vuelvo a mandarel mismo)
+	cohete -> bytes
+	
+	trama= seial.leerHasta('#')
+	SI trama[0] == '$'
+	long
+	[1], proto, = pkt
+	48 -> datos plot,
+*/
+
+
 bool LoraWrapped::send_package(pkt_t * ptrPkt){
     char encryptedByte = 0x00;
     
@@ -66,13 +97,13 @@ bool LoraWrapped::read_package(pkt_t *ptrPkt){
     }
 
     // Asigna el puntero de la union según protocolo
-    if(ptrPkt->protocole  == PLOT ){
+    if(ptrPkt->protocole  == C_PLOT ){
         memset( (void*)&_internalPayload.data, 0, sizeof(dataPlot_t));
-        ptrPkt->payload = (dataPlot_t*) &_internalPayload.data;
+        ptrPkt->payload = (dataPlot_t*) &_internalPayload.data; 
     }
     /* MSG, ERR : Mensajes de error de longitud 128 bytes, incluido el '\0' */
     else{
-        memset( (void*)_internalPayload.msg, 0, SIZE_BUFFER_MSG);
+        memset( (void*)_internalPayload.msg, 0, SIZE_BUFFER_MSG); // 128 
         ptrPkt->payload = (char*)_internalPayload.msg;
     }
     // ptrPkt->payload = PLOT? (dataPlot_t*) &payload.data: (char*)payload.msg;
@@ -86,7 +117,7 @@ bool LoraWrapped::read_package(pkt_t *ptrPkt){
     return true;
 }
 
-bool LoraWrapped::connect_to_GSE(){
+bool LoraWrapped::c_connect_to_GSE(){
     pkt_t paquete;
     char *msg = "PING_COHETE";
     
@@ -102,7 +133,7 @@ bool LoraWrapped::connect_to_GSE(){
 }
 
 
-bool LoraWrapped::accept_connection(){
+bool LoraWrapped::g_accept_connection(){
     pkt_t paqueteRecibido;
     pkt_t paqueteRespuesta;
     // mensaje de respuesta(PONG)
@@ -128,13 +159,14 @@ bool LoraWrapped::accept_connection(){
         paqueteRespuesta.len = strlen(respuesta) + 1;
 
         // Envia la confirmación
+        _st = CONNECTION_STATUS::CONNECTED;
         return send_package(&paqueteRespuesta);
     }
     return false;
 }
 
 
-bool LoraWrapped::connection_isAccepted() {
+bool LoraWrapped::c_connection_accepted() {
     pkt_t paquete;
     
     // Verificamos si llegó algo
@@ -142,6 +174,7 @@ bool LoraWrapped::connection_isAccepted() {
         // El cohete espera un PONG para confirmar la conexión
         if (paquete.protocole == Protocolo::PONG) {
             // un flag de "ENLACE COMPLETADO"
+            _st = CONNECTION_STATUS::CONNECTED;
             return true;
         }
     }
@@ -152,13 +185,17 @@ bool LoraWrapped::connection_isAccepted() {
 
 bool LoraWrapped::send_datos(dataPlot_t datos) {
     pkt_t paquete;
-    
+
+    // Verifica conexion
+    if(_st !=CONNECTION_STATUS::CONNECTED) return false;
     _internalPayload.data = datos;
     
-    paquete.protocole = Protocolo::PLOT;
+    // Prepara el paquete
+    paquete.protocole = Protocolo::C_PLOT;
     paquete.payload = &_internalPayload.data;
     paquete.len = sizeof(dataPlot_t);
 
+    // Envia el paquete
     return send_package(&paquete);
 }
 
@@ -166,14 +203,16 @@ bool LoraWrapped::send_datos(dataPlot_t datos) {
 bool LoraWrapped::send_mensaje(const char* texto) {
     pkt_t paquete;
 
-    if (texto == nullptr) return false;
-    
+    // verifica la conexion o si existe el mensaje
+    if (texto == nullptr || _st !=CONNECTION_STATUS::CONNECTED){
+        return false;
+    }
     // Copia el texto al buffer de la unión
     strncpy(_internalPayload.msg, texto, SIZE_BUFFER_MSG);
     _internalPayload.msg[SIZE_BUFFER_MSG - 1] = '\0'; // Asegurar cierre de cadena
 
-    // Configura el paquete
-    paquete.protocole = Protocolo::MGS; 
+    // Configura el paquete de mensaje
+    paquete.protocole = Protocolo::C_MGS; 
     paquete.payload = _internalPayload.msg;
     paquete.len = strlen(_internalPayload.msg) + 1; // +1 para incluir el '\0'
 
@@ -184,14 +223,14 @@ bool LoraWrapped::send_mensaje(const char* texto) {
 bool LoraWrapped::send_mensaje_error(const char* error) {
     pkt_t paquete;
 
-    if (error == nullptr) return false;
+    if (error == nullptr || _st !=CONNECTION_STATUS::CONNECTED) return false;
     
     // Copia el error al buffer
     strncpy(_internalPayload.msg, error, SIZE_BUFFER_MSG);
     _internalPayload.msg[SIZE_BUFFER_MSG - 1] = '\0';
 
     // Configuramos el paquete como error
-    paquete.protocole = Protocolo::ERR;
+    paquete.protocole = Protocolo::C_ERR;
     paquete.payload = _internalPayload.msg;
     paquete.len = strlen(_internalPayload.msg) + 1;
 
--- lib/LoraWrapped/LoraWrapped.h
+++ lib/LoraWrapped/LoraWrapped.h
@@ -1,3 +1,8 @@
+/*
+
+* @details: c_: funciones o comandos que utiliza el Cohete/CPU/Copm...
+			g_: Funciones o comandos que utiliza el GSE
+*/ 
 #ifndef LORAWRAPPED_H
 #define LORAWRAPPED_H
 
@@ -19,8 +24,14 @@ typedef struct PAQUETE {
 
 } pkt_t;
 
+
+
+/* PROVI VA EN GLOALS.H*/
 typedef struct {
-    int dato[8];
+    int datoX;
+    float giroX;
+    float giroY;
+    float altitud;
 }dataPlot_t;
 
 union pay_u {
@@ -30,14 +41,14 @@ union pay_u {
 
 enum Protocolo: uint8_t {
     // CPU --> GSE
-    PLOT    = 0X01,
-    MGS     = 0X02,
-    ERR     = 0X03,
+    C_PLOT    = 0X01,
+    C_MGS     = 0X02,
+    C_ERR     = 0X03,
     PING    = 0X04,
 
     // GSE --> CPU
-    START   = 0X10,
-    END     = 0X20,
+    G_START   = 0X10,
+    G_END     = 0X20,
     PONG    = 0X30
 };
 
@@ -45,23 +56,47 @@ enum Protocolo: uint8_t {
 class LoraWrapped
 {
 private:
+    /* estados de la conexcion, para mejorar la reconeccion, proximamente*/
+    typedef enum CONNECTION_STATUS: uint8_t {DISCONNECTED = 0x00, CONNECTED} connSts_t; /*CONNECTION_LOST */
+    /* Palabra de sincronización con el modulo a comunicar, evita interferencias de otro módulo*/
     int _syncWord;
+
+    /* caracter de encriptación, en caso de interseeción espía XD*/
     char _encryptWord;
+
+    /* Buffer interno para enviar los datos de manera segura*/
     pay_u _internalPayload;
 
+    /* Estado de la conexion*/
+    connSts_t _st;
+
+    /* Funciones de bajo nivel, des/encriptación y envio*/
     bool send_package(pkt_t *ptrPkt);
     bool read_package(pkt_t *ptrPkt);
+
     /* Cifrado de un byte con el método XOR*/
     inline char encrypt_byte(char dat) {return  ((char)dat)^_encryptWord ;};
 public:
     LoraWrapped(int ss, int reset, int dio0, SPIClass& spi = DEFAULT_SPI_LORA);
     ~LoraWrapped();
     bool begin(int sw = DEFAULT_SYNC_WORD, char ew = DEFAULT_ENCRY_WORD, long frequency = DEFAULT_FREC);
-    bool connect_to_GSE();
-    bool connection_isAccept();
-    bool accept_connection();
+    
+    /**
+     * @brief Envia una petición de conexion del cohete al GSE
+     */
+    bool c_connect_to_GSE();
 
-    bool connection_isAccepted();
+    /**
+     * @brief Verifica la conexion estable con el GSE
+     */
+    bool c_connection_accepted();   
+    
+    /** 
+     *   @brief Acepta la petición de conexion del cohete
+     */
+    bool g_accept_connection();
+
+
 
     /**
      * @brief Envía datos de telemetría (struct dataPlot_t)
@@ -80,4 +115,33 @@ public:
 };
 
 
-#endif /* LORAWRAPPED_H */
+#endif /* LORAWRAPPED_H **/
+
+
+/*
+[ msg / plot
+X Y >
+x Y >
+x Y >
+x Y >
+x   >
+x   >
+]
+
+
+main:
+------
+muy rapido 5 ms no resolucion -> fiabilidad 
+sensor-> cola(muuuuuuuuy) [prol, lon, dato]
+------
+lora.ocupado?
+    NO:
+	enviar segun protocolo: plot, msg <-- paquete <-- cola
+end
+-------
+mde:
+----
+mde_envio
+---
+--
+*/
